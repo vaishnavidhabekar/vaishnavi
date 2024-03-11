b86070fc5624660af620dd8fc7cd02a1bc555214
vaishnavidhabekar99 <vaishnavidhabekar99@gmail.com>
2024-03-11 14:11:47 +0530

    icon, profile constant declear,

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 
+import { Profile } from '../constants/app-constant';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
 
+export class Tab1Page {
+  public profileDetails = Profile;
+  
   constructor() {}
 
 }
