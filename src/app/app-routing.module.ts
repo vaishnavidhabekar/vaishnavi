7ae015acc19a55537a6887e02172086fad349082
vaishnavidhabekar <140101981+vaishnavidhabekar@users.noreply.github.com>
2024-03-20 10:01:55 +0530

    Email resume download (#4)
    * add botton download , email
    
    * header contain change, color change
    
    * create componets
    
    * creat component pages ,creat sub component ,add menu function on home pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./pages/about/about.module').then( m => m.AboutPageModule)
+  },
+  {
+    path: 'resume',
+    loadChildren: () => import('./pages/resume/resume.module').then( m => m.ResumePageModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./pages/contact/contact.module').then( m => m.ContactPageModule)
   }
+
 ];
 @NgModule({
   imports: [
