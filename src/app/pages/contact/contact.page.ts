868ea83901a16fff0314749edacc709e05de9c75
vaishnavidhabekar99 <vaishnavidhabekar99@gmail.com>
2024-04-02 12:01:38 +0530

    change background image home page ,contact page add reactive forms

--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import { ToastController } from '@ionic/angular';
 import { Profile } from 'src/app/constants/app-constant';
-
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.page.html',
@@ -8,9 +14,30 @@ import { Profile } from 'src/app/constants/app-constant';
 })
 export class ContactPage implements OnInit {
   public profileDetails = Profile;
-  constructor() { }
 
-  ngOnInit() {
+  public contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder, public toastController:ToastController) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', [Validators.required]],
+      message: ['', [Validators.required]],
+    });
   }
-
+  ngOnInit(){
+    
+  }
+  async submitRespose(){
+ 
+    const toast = await this.toastController.create({
+      
+      message: 'Thank You For Feedback',
+      duration: 4000,
+      buttons:["ok"]
+    });
+    await toast.present();
+  
+  }
+ 
 }
