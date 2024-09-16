ec3a7abb6a67c9d76215c232a569b3fd409e60f9
vaishnavidhabekar99 <vaishnavidhabekar99@gmail.com>
2024-09-16 12:26:00 +0530

    change resume folder,add home page

--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import { ToastController } from '@ionic/angular';
+import { Profile } from 'src/app/constants/app-constant';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.page.html',
   styleUrls: ['./contact.page.scss'],
 })
 export class ContactPage implements OnInit {
+  public profileDetails = Profile;
 
-  constructor() { }
+  public contactForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder, public toastController:ToastController) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', [Validators.required]],
+      message: ['', [Validators.required]],
+    });
+  }
+  ngOnInit(){
+    
+  }
+  async submitRespose(){
+ 
+    const toast = await this.toastController.create({
+      
+      message: 'Thank You For Feedback',
+      duration: 4000,
+      buttons:["ok"]
+    });
+    await toast.present();
+  
   }
  
 }
